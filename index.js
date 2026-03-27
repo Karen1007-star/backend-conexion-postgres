e185b547902d205f90154e373babdfa78bb95e6a
Selene <karen_08_121@hotmail.com>
2026-03-27 18:15:15 -0500

    Empezando el CRUD de tareas

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const pool = require("./db");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+///// get
+app.get("/tareas", async (req, res) => {
+  try {
+    // const time = await pool.query("SELECT NOW()");
+    const tareas = await pool.query('SELECT * FROM tareas');
+
+    res.json({
+    //   serverTime: time.rows[0],
+      tareas: tareas.rows
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ mensaje: "Error conectando a PostgreSQL" });
+  }
+});
+
+//// Post
+app.post("/tareas", async (req,res)=>{
+    try {
+      const { titulo, completado } = req.body;
+      const crearTareas = await pool.query("INSERT INTO tareas (titulo, completado) VALUES ($1, $2) RETURNING *", [titulo, completado]);
+      res.json(crearTareas.rows[0])
+    } catch (error) {
+      res.status(500).send({mensaje:"No se pudo crear tarea"})
+    }
+})
+
+//// Patch
+app.patch("/tareas/:id", async (req,res)=>{
+  try {
+    const id = Number(req.params.id);
+    const { titulo, completado } = req.body;
+    tarea = tarea.map((ta)=>{
+        if(ta.id===id){
+           return{
+            ...ta,
+            ...(titulo!=undefined ? {titulo} : {}),
+            ...(completado!=undefined ? {completado:!completado} : {})
+           }
+        }
+      return ta  
+    })
+  } catch (error) {
+    res.status(500).send({mensaje:"No se pudo modificar tarea"})
+  }
+})
+
+app.listen(3000, () => {
+  console.log("🟢 Servidor corriendo en http://localhost:3000");
+});
